80b99981c74a08215b03db9ea17e5572becc6a83
aali2537 <alexander_li1@student.uml.edu>
2021-09-30 19:22:26 -0400

    Functionality of radio buttons

--- src/RadioButtons/RadioButtons.js
+++ src/RadioButtons/RadioButtons.js
@@ -1,9 +1,52 @@
-import { Component } from 'react';
-import './RadioButtons.css';
+import { Component } from "react";
+import Button from "components/Button/Button";
+import Panel from "components/Panel/Panel";
+import "./RadioButtons.css";
 
 class RadioButtons extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      options: ["Apple", "Pear", "Orange"],
+      currentlySelected: "Apple",
+      disabled: { Orange: true }
+    };
+  }
+
+  clickHandler = (e) => {
+    this.setState((prevState) => ({
+      currentlySelected: e.target.textContent
+    }));
+  };
+
   render() {
-    return null;
+    return (
+      <Panel>
+        {this.state.options.map((option) => {
+          let buttonClass = "Default-button";
+          let buttonDisabled = false;
+
+          if (option === this.state.currentlySelected) {
+            buttonClass = "Default-button Btn-Selected";
+          }
+
+          if (this.state.disabled[option]) {
+            buttonDisabled = true;
+          }
+
+          return (
+            <Button
+              text={option}
+              key={option}
+              onClick={this.clickHandler}
+              className={buttonClass}
+              disabled={buttonDisabled}
+            ></Button>
+          );
+        })}
+      </Panel>
+    );
   }
 }
 
